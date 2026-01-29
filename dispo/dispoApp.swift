9ff33e0a378afb04b8d08f4249b2d5f1a449d094
Seth Schallau <seth.schallau@vitalsource.com>
2026-01-28 23:18:54 -0500

    Initial Commit

---
+++ dispo/dispoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dispoApp.swift
+//  dispo
+//
+//  Created by Seth Schallau on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct dispoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
